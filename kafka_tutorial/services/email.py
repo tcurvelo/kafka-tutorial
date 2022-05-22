a12f093da045b64fdcf6e239b451ba636db5c7cb
Thiago Curvelo <tcurvelo@gmail.com>
2022-05-22 20:00:41 -0300

    Add a EmailService

---
+++ kafka_tutorial/services/email.py
@@ -0,0 +1,22 @@
+import time
+
+from kafka_tutorial import settings
+from kafka_tutorial.services import KafkaService
+
+
+class EmailService(KafkaService):
+    def __init__(self):
+        super().__init__(topics=[settings.ECOMMERCE_SEND_EMAIL], extras=["email"])
+
+    def parse(self, msg):
+        print(
+            "📬 Sending email: "
+            f"{msg.key().decode()}: "
+            f"{msg.value().decode()} ({msg.partition()}, {msg.offset()})"
+        )
+        time.sleep(2)
+        print("Email sent")
+
+
+if __name__ == "__main__":
+    EmailService()()
