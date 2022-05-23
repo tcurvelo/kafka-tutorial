12dcdd71fcba61f6ecd610483aaaa8d54cbec4b1
Thiago Curvelo <tcurvelo@gmail.com>
2022-05-22 23:16:41 -0300

    FraudDetector: service also act as producer

--- kafka_tutorial/services/fraud_detector.py
+++ kafka_tutorial/services/fraud_detector.py
@@ -2,7 +2,7 @@ import time
 
 from kafka_tutorial import settings
 from kafka_tutorial.order import key_deserializer, value_deserializer
-from kafka_tutorial.services import KafkaService
+from kafka_tutorial.services import KafkaDispatcher, KafkaService
 
 
 class FraudDetectorService(KafkaService):
@@ -16,6 +16,7 @@ class FraudDetectorService(KafkaService):
                 "value.deserializer": value_deserializer,
             },
         )
+        self.dispatcher = KafkaDispatcher()
 
     def parse(self, msg):
         print(
@@ -23,8 +24,22 @@ class FraudDetectorService(KafkaService):
             f"{msg.key()}: {msg.value()} ({msg.partition()}, {msg.offset()})"
         )
         time.sleep(5)
-        print("Order processed")
+
+        order = msg.value()
+        if self.is_fraud(order):
+            print("💀 Order is a fraud!")
+            self.dispatcher.send(
+                settings.Topic.ECOMMERCE_ORDER_REJECTED, order.user_id, order
+            )
+        else:
+            print("✔️ Order processed")
+            self.dispatcher.send(settings.ECOMMERCE_ORDER_APROVED, order.user_id, order)
+
         print("------------------------------------------")
+
+    def is_fraud(self, order):
+        # pretending that the fraud happens when amount > 4500
+        return order.amount > 4500
 
 
 if __name__ == "__main__":
