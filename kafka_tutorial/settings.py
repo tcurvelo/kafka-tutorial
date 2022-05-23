bdc68f12f4ff2de887b5bfc230f225158bd3b871
Thiago Curvelo <tcurvelo@gmail.com>
2022-05-22 23:13:40 -0300

    Add a `Topic` enum

--- kafka_tutorial/settings.py
+++ kafka_tutorial/settings.py
@@ -1,8 +1,17 @@
 from configparser import ConfigParser
+from enum import Enum
 
-ECOMMERCE_NEW_ORDER = "ECOMMERCE_NEW_ORDER"
-ECOMMERCE_SEND_EMAIL = "ECOMMERCE_SEND_EMAIL"
-ECOMMERCE_ALL = "^ECOMMERCE_.*"
+
+class Topic(Enum):
+    ECOMMERCE_ALL = "^ECOMMERCE_.*"
+    ECOMMERCE_NEW_ORDER = "ECOMMERCE_NEW_ORDER"
+    ECOMMERCE_SEND_EMAIL = "ECOMMERCE_SEND_EMAIL"
+    ECOMMERCE_ORDER_APROVED = "ECOMMERCE_ORDER_APROVED"
+    ECOMMERCE_ORDER_REJECTED = "ECOMMERCE_ORDER_REJECTED"
+
+    @classmethod
+    def list_valid_topics(cls):
+        return [e.value for e in cls if not e.value.startswith("^")]
 
 
 def get_config(extras=None):
@@ -16,3 +25,7 @@ def get_config(extras=None):
                 config.update(dict(config_parser[section]))
 
     return config
+
+
+if __name__ == "__main__":
+    print("\n".join(Topic.list_valid_topics()))
